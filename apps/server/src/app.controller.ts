fe45ded760c9a7f4ecd83510884bbf4a4b7e7084
Blossssom <eju2003@gmail.com>
2025-09-09 16:04:45 +0900

    Feat: Add env file, file path for nest server config
    - 진행 중
    - 서버 config를 위한 env 설정 및 파일 설정 중

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    console.log(process.env.APP_TEST);
+    return 'hello';
   }
 }
--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { getEnvFilePath } from './utils/env-file-path';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: getEnvFilePath(),
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
